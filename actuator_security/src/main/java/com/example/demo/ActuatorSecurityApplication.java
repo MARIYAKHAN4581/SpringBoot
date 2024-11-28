02883455955c11427da138af6ec72af784d6203f
MARIYAKHAN4581 <mariyakhan4581@gmail.com>
2024-11-28 16:58:25 +0530

    using actuator security

---
+++ actuator_security/src/main/java/com/example/demo/ActuatorSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorSecurityApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
